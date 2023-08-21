9c1155df4250ffaca56ec71d28bd7724730359e3
naomicrisp <naomicrisp2@gmail.com>
2023-08-21 14:16:12 +0100

    Initial app structure set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  async function displayNumbers() {
+    try {
+      // Fetch numbers from local API
+      const response = await fetch("http://localhost:3000/numbers"); // Use "http" instead of "https"
+      const numbers = await response.json();
+      
+      // Display numbers in browser, targeting the <p> tag with id="numbers"
+      const numbersElement = document.getElementById("numbers");
+      numbersElement.innerHTML = numbers.join(", ");
+    } catch (error) {
+      console.error("Error fetching numbers:", error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FizzBuzz</h1>
+      <p>Click the button to make fizz buzz happen </p>
+      <button onClick={displayNumbers}>Click Me</button>
+      <p id="numbers"></p>
+
     </div>
   );
 }
---
+++ src/fizzbuzzAPI/fizzbuzz.js
@@ -0,0 +1,35 @@
+import fs from "node:fs/promises";
+const fileName = "./numbers.json"
+
+  // Main function to generate number list
+export async function writeNumbers() {
+
+// STEP 1: create a function to create list of numbers 1-100 and write to numbers.json
+    const numbers = Array.from({ length: 100 }, (_, i) => i + 1);
+
+// STEP 2: Replace multiples of 3 and 5 with FizBuzz
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0) {
+        numbers[i] = "FizzBuzz";
+    }
+
+// STEP 3: Replace multiples of only 3 with Fizz
+    else if (numbers[i] % 3 === 0) {
+            numbers[i] = "Fizz";
+        }
+
+// STEP 4: replace multiples of only 5 with Buzz
+    else if (numbers[i] % 5 === 0) {
+            numbers[i] = "Buzz";
+        }
+
+    else numbers[i] = numbers[i]
+    }  
+
+
+// STEP 5: write the numbers to numbers.json
+
+    await fs.writeFile(fileName, JSON.stringify(numbers));
+  
+    return numbers;
+  }
