e589e31d8251d2871437f300c3e25b82e8131888
naomicrisp <naomicrisp2@gmail.com>
2023-08-21 14:36:43 +0100

    line by line styling added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
+import { React, useState } from "react";
 
 function App() {
-
+  const [numbersList, setNumbersList] = useState([]);
+ 
   async function displayNumbers() {
     try {
       // Fetch numbers from local API
       const response = await fetch("http://localhost:3000/numbers"); // Use "http" instead of "https"
       const numbers = await response.json();
-      
+      setNumbersList(numbers);
+    
       // Display numbers in browser, targeting the <p> tag with id="numbers"
-      const numbersElement = document.getElementById("numbers");
-      numbersElement.innerHTML = numbers.join(", ");
-    } catch (error) {
-      console.error("Error fetching numbers:", error);
+    //   const numbersElement = document.getElementById("numbers");
+    //   numbersElement.innerHTML = numbers.join("\n");
+    } 
+    catch (error) {
+    console.error("Error fetching numbers:", error);
     }
   }
 
@@ -20,9 +24,14 @@ function App() {
     <div className="App">
       <h1>FizzBuzz</h1>
       <p>Click the button to make fizz buzz happen </p>
-      <button onClick={displayNumbers}>Click Me</button>
-      <p id="numbers"></p>
-
+      <button onClick={displayNumbers}>FizzBuzz</button>
+     
+    
+        { numbersList && numbersList.map((number, index) => (
+          <p key={index}>{number}</p>
+        ))}
+    
+  
     </div>
   );
 }
